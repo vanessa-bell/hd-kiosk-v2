1120f65eaeab62d0ac0c89c7f5ac33c1714610d4
Vanessa Bell <vanessa.bell14@gmail.com>
2017-05-15 22:12:27 -0700

    events page works on local machine but not when deployed. solution may be to use urllib3

--- main/events.py
+++ main/events.py
@@ -1,16 +1,41 @@
-import flask
+# import flask
 
-import model
-import util
+# from datetime import date, datetime, timedelta
 
-from main import app
+# import requests
+# import requests_toolbelt.adapters.appengine
+# import json
 
-@app.route('/events/')
+# from main import app
 
-def events():
+# # Use the App Engine Requests adapter. This makes sure that Requests uses
+# # URLFetch.
+# requests_toolbelt.adapters.appengine.monkeypatch()
 
-  return flask.render_template(
-      'events.html',
-      html_class='events',
-      title='Upcoming Events',
-    )
+# @app.route('/events/')
+
+# def events():
+# 	url = 'https://events.hackerdojo.com/events.json'
+# 	response = requests.get(url=url)
+# 	data = response.json()
+	
+# 	yesterday = datetime.strftime(date.today() - timedelta(1), "%A, %B %d, %Y")
+# 	today = datetime.strftime(date.today(), "%A, %B %d, %Y")
+# 	print(yesterday)
+
+# 	for event in data:
+# 		event['start_time'] = datetime.strptime(event['start_time'], '%Y-%m-%dT%H:%M:%S')
+# 		event['end_time'] = datetime.strptime(event['end_time'], '%Y-%m-%dT%H:%M:%S')
+# 		event['day_raw'] = event['start_time'].strftime("%j")
+# 		event['day'] = event['start_time'].strftime("%A, %B %d, %Y")
+# 		event['start'] = event['start_time'].strftime("%I:%M %p")
+# 		event['end'] = event['end_time'].strftime("%I:%M %p")
+
+# 	return flask.render_template(
+# 		'events.html',
+# 		html_class='events',
+# 		title='Upcoming Events',
+# 		data=data,
+# 		yesterday=yesterday,
+# 		today=today
+# 	)
